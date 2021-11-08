e017d11689cd0fce639b12acb21c5489af6099ad
WaltVenditi <waltvenditti@gmail.com>
2021-11-08 17:49:08 -0500

    added keyboard support, some style, done with it

--- scripts.js
+++ scripts.js
@@ -154,7 +154,7 @@ function toggleClear() {
 };
 
 
-// button listeners
+// isteners
 //-----------------
 
 const btn0 = document.querySelector('#b0');
@@ -426,8 +426,327 @@ btnEq.addEventListener('click', () => {
 });
 
 
+// keyboard support
+//------------------------------------------------
+//------------------------------------------------
+//------------------------------------------------
+//------------------------------------------------
 
-// C on start, on right after equal, on hitting second operator in a chain calculation (i.e. after calcandupdate and calcandupdatenoy)
+// number keys
+//------------------------------------------------
 
-// toggle CE upon every number click, or op click
-// toggle C after each computation 
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '0') {
+        if (clearState === true) toggleClear();
+        if (firstPress === true) {
+            displayNumber = '0';
+            firstPress = false;
+        } else {
+            displayNumber += '0';
+        }
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '1') {
+        if (clearState === true) toggleClear();
+        if (firstPress === true) {
+            displayNumber = '1';
+            firstPress = false;
+        } else {
+            displayNumber += '1';
+        }
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '2') {
+        if (clearState === true) toggleClear();
+        if (firstPress === true) {
+            displayNumber = '2';
+            firstPress = false;
+        } else {
+            displayNumber += '2';
+        }
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '3') {
+        if (clearState === true) toggleClear();
+        if (firstPress === true) {
+            displayNumber = '3';
+            firstPress = false;
+        } else {
+            displayNumber += '3';
+        }
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '4') {
+        if (clearState === true) toggleClear();
+        if (firstPress === true) {
+            displayNumber = '4';
+            firstPress = false;
+        } else {
+            displayNumber += '4';
+        }
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '5') {
+        if (clearState === true) toggleClear();
+        if (firstPress === true) {
+            displayNumber = '5';
+            firstPress = false;
+        } else {
+            displayNumber += '5';
+        }
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '6') {
+        if (clearState === true) toggleClear();
+        if (firstPress === true) {
+            displayNumber = '6';
+            firstPress = false;
+        } else {
+            displayNumber += '6';
+        }
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '7') {
+        if (clearState === true) toggleClear();
+        if (firstPress === true) {
+            displayNumber = '7';
+            firstPress = false;
+        } else {
+            displayNumber += '7';
+        }
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '8') {
+        if (clearState === true) toggleClear();
+        if (firstPress === true) {
+            displayNumber = '8';
+            firstPress = false;
+        } else {
+            displayNumber += '8';
+        }
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '9') {
+        if (clearState === true) toggleClear();
+        if (firstPress === true) {
+            displayNumber = '9';
+            firstPress = false;
+        } else {
+            displayNumber += '9';
+        }
+        updateDisplay();
+    }
+});
+
+
+// the operator keys
+// -----------------
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '+') {
+        if (clearState === true) toggleClear();
+        if (displayNumber === '0' && firstPress === true) return;
+        if (checkOpSwitch() === true) removeOperator();
+        if (!(x === null)) {
+            calcAndUpdate();
+            firstPress = false;
+        }
+        x = Number(displayNumber);
+        displayNumber += ' + ';
+        operator = '+';
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '-') {
+        if (clearState === true) toggleClear();
+        if (displayNumber === '0' && firstPress === true) return;
+        if (checkOpSwitch() === true) removeOperator();
+        if (!(x === null)) {
+            calcAndUpdate();
+            firstPress = false;
+            }
+        x = Number(displayNumber);
+        displayNumber += ' - ';
+        operator = '-';
+        updateDisplay();
+    };
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '*') {
+        if (clearState === true) toggleClear();
+        if (displayNumber === '0' && firstPress === true) return;
+        if (checkOpSwitch() === true) removeOperator();
+        if (!(x === null)) {
+            calcAndUpdate();
+            firstPress = false;
+            }
+        x = Number(displayNumber);
+        displayNumber += ' \xD7 ';
+        operator = '*';
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === '/') {
+        if (clearState === true) toggleClear();
+        if (displayNumber === '0' && firstPress === true) return;
+        if (checkOpSwitch() === true) removeOperator();
+        if (!(x === null)) {
+            calcAndUpdate();
+            firstPress = false;
+            }
+        x = Number(displayNumber);
+        displayNumber += ' \xF7 ';
+        operator = '/';
+        updateDisplay();
+    }
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+        if (e.key === '.') {
+        if (displayNumber.split(' ')[2] === '') {
+            displayNumber += '0.';
+        } else {
+            displayNumber += '.';
+        }
+        updateDisplay();
+    }
+});
+
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+        if (clearState === true) {
+            clearData();
+            firstPress = true;
+            xForRepeat = null;
+            yForRepeat = null;
+            opForRepeat = null;
+            updateDisplay();
+        } else {
+            numArray = displayNumber.split(' ');
+            arrayLen = numArray.length;
+            if (arrayLen === 1) {
+                if (numArray[0].length > 1) {
+                    displayNumber = displayNumber.slice(0, -1);
+                    updateDisplay();
+                    return;
+                } else if (!(numArray[0] === '0')) {
+                    displayNumber = '0';
+                    updateDisplay();
+                    firstPress = true;
+                    toggleClear();
+                    xForRepeat = null;
+                    yForRepeat = null;
+                    opForRepeat = null;
+        
+                    return;
+                } else if (numArray[0] === '0') {
+                    if (clearState === false) toggleClear();
+                }
+            };
+            if (arrayLen === 3) {
+                if (!(numArray[2] === '')) {
+                    displayNumber = displayNumber.slice(0, -1);
+                    updateDisplay();
+                } else {
+                    displayNumber = displayNumber.slice(0, -3);
+                    x = null;
+                    operator = null;
+                    updateDisplay();
+                }
+            };45
+        }
+    }
+});
+
+
+btnC.addEventListener('click', () => {
+    clearData();
+    firstPress = true;
+    xForRepeat = null;
+    yForRepeat = null;
+    opForRepeat = null;
+    updateDisplay();
+});
+
+btnCE.addEventListener('click', () => {
+    numArray = displayNumber.split(' ');
+    arrayLen = numArray.length;
+    if (arrayLen === 1) {
+        if (numArray[0].length > 1) {
+            displayNumber = displayNumber.slice(0, -1);
+            updateDisplay();
+            return;
+        } else if (!(numArray[0] === '0')) {
+            displayNumber = '0';
+            updateDisplay();
+            firstPress = true;
+            toggleClear();
+            xForRepeat = null;
+            yForRepeat = null;
+            opForRepeat = null;
+
+            return;
+        } else if (numArray[0] === '0') {
+            if (clearState === false) toggleClear();
+        }
+    };
+    if (arrayLen === 3) {
+        if (!(numArray[2] === '')) {
+            displayNumber = displayNumber.slice(0, -1);
+            updateDisplay();
+        } else {
+            displayNumber = displayNumber.slice(0, -3);
+            x = null;
+            operator = null;
+            updateDisplay();
+        }
+    };
+});
+
+document.documentElement.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        if (x === null) {
+            if (!(xForRepeat === null)) {
+                x = xForRepeat;
+                y = yForRepeat;
+                operator = opForRepeat;
+                calcAndUpdateNoY();
+                return;
+            } else return; 
+        }
+        calcAndUpdate();
+    }
+});
